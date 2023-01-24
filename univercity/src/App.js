fa961c9a51b3abb0b35e98507c4f2e3e69d7cd5a
Bastien <neitsabast@gmail.com>
2023-01-24 10:47:46 +0100

    initial commit

--- univercity/src/App.js
+++ univercity/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Projet de fou
         </a>
       </header>
     </div>
